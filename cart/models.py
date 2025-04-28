e077e78f54d7e8688eaec8551be123576a19bca9
bendamian <badamian@gmail.com>
2025-04-28 14:05:27 +0100

    made changes to book model

--- cart/models.py
+++ cart/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.utils.timezone import datetime
 from django.contrib.auth.models import User
-from solo.models import Book  # my Book model is in the 'solo' app
 
+from solo.models import Book  # my Book model is in the 'solo' app
+import uuid
 
 class CartItem(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -16,27 +18,27 @@ class CartItem(models.Model):
         return self.quantity * self.book.price
 
 
-class Order(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    shipping_address = models.TextField()
-    phone_number = models.CharField(max_length=20)
-    paid = models.BooleanField(default=False)
-
-    def __str__(self):
-        return f'Order {self.id}'
-
-
 class OrderItem(models.Model):
-    order = models.ForeignKey(
-    Order, related_name='items', on_delete=models.CASCADE)
-    book = models.ForeignKey(Book, on_delete=models.CASCADE)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    item = models.ForeignKey(Book,on_delete=models.CASCADE)
+    ordered_date = models.DateTimeField('date ordered')
     quantity = models.PositiveIntegerField(default=1)
 
     def __str__(self):
-        return f'{self.quantity} x {self.book.title}'
+        return f'{self.quantity} x {self.item.title}'
 
 
+class Order(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='orders')
+    ref_code = models.UUIDField(
+        default=uuid.uuid4, editable=False, unique=True)
+    items = models.ManyToManyField('OrderItem', related_name='orders')
+    created_at = models.DateTimeField(auto_now_add=True)
+    ordered_date = models.DateTimeField()
+    ordered = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'Order {self.ref_code} by {self.user.username}'
 
 
--- solo/models.py
+++ solo/models.py
@@ -3,12 +3,28 @@ from django.utils.text import slugify
 from django.urls import reverse
 
 
+type_choices = (
+    ('New', 'New'),
+    ('Bestseller', 'Bestseller'),
+    ('Offers', 'On offers'),
+)
+
+LABEL_CHOICES = (
+    ('P', 'primary'),
+    ('S', 'secondary'),
+    ('D', 'danger')
+)
+
+
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True, blank=True)
     author = models.CharField(max_length=100, blank=True)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    type = models.CharField(max_length=20, choices=type_choices, blank=True)
+    label = models.CharField(max_length=1, choices=LABEL_CHOICES, blank=True)
     cover = models.ImageField(upload_to='book_covers/', blank=True, null=True)
 
     def __str__(self):
