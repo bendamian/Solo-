6234749efd9f797653b48df24127a593d386d97f
bendamian <badamian@gmail.com>
2025-04-30 23:06:08 +0100

    improve navbar

--- cart/models.py
+++ cart/models.py
@@ -5,19 +5,6 @@ from django.contrib.auth.models import User
 from solo.models import Book  # my Book model is in the 'solo' app
 import uuid
 
-class CartItem(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    book = models.ForeignKey(Book, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField(default=1)
-    added_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"{self.quantity} x {self.book.title}"
-
-    def get_total_price(self):
-        return self.quantity * self.book.price
-
-
 class OrderItem(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     book = models.ForeignKey(Book,on_delete=models.CASCADE)
@@ -43,3 +30,18 @@ class Order(models.Model):
         return f'Order {self.ref_code} by {self.user.username}'
 
 
+'''
+# test model
+class CartItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    added_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.quantity} x {self.book.title}"
+
+    def get_total_price(self):
+        return self.quantity * self.book.price
+
+'''
