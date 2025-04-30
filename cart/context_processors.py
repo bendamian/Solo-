d6ef9ec54cf285c80024e3392d61fd1ba58e5de3
bendamian <badamian@gmail.com>
2025-04-30 22:52:21 +0100

    fix the context processor

--- cart/context_processors.py
+++ cart/context_processors.py
@@ -1,10 +1,10 @@
 # cart/context_processors.py
-from .models import CartItem
+from .models import Order
 
 
 def cart_item_count(request):
     if request.user.is_authenticated:
-        count = CartItem.objects.filter(user=request.user).count()
-    else:
-        count = 0
-    return {'cart_item_count': count}
+        order = Order.objects.filter(user=request.user, ordered=False).first()
+        if order:
+            return {'cart_item_count': sum(item.quantity for item in order.items.all())}
+    return {'cart_item_count': 0}
